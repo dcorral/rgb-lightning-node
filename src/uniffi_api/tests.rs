069703c459d13443f15abbd906f6540c92c39f93
dcorral <hi@dcorral.com>
2026-04-15 14:05:05 +0200

    Fix UniFFI smoke tests

--- src/uniffi_api/tests.rs
+++ src/uniffi_api/tests.rs
@@ -93,6 +93,8 @@ mod uniffi_smoke_tests {
                 enable_virtual_channels_v0: false,
                 virtual_peer_pubkeys: vec![],
                 database: Arc::new(database),
+                vss_url: None,
+                vss_unencrypted: false,
             }),
             cancel_token: CancellationToken::new(),
             unlocked_app_state: Arc::new(TokioMutex::new(None)),
